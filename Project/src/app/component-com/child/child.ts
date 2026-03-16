e4d7927731426cd244565b082883024bef005f91
Patel Urmi <129217613+UrmiPatel-002772667@users.noreply.github.com>
2026-03-16 12:37:29 +0530

    Add files via upload

---
+++ Project/src/app/component-com/child/child.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  imports: [],
+  templateUrl: './child.html',
+  styleUrl: './child.css',
+})
+export class Child {
+  @Input() childInputProperty : string = "";
+  @Output() childOutputProperty = new EventEmitter<any>();
+
+  sendData(){
+    this.childOutputProperty.emit("This is child component Data.");
+  }
+}
+
